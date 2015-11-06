ff87705c6b60c05599adfea50efa78f9bf6be2c6
Administrator <admin@myself.local>
2015-11-06 10:59:59 -0500

    added theater

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :movie_store, MovieStore.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "admin",
+  password: "",
   database: "movie_store_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
